ffcde2ae6d4ee3681fa7afc55f571967d74dd426
Trevor Adams <nytadams@familysearch.org>
2018-02-12 09:38:53 -0700

    Removing test on IE (woot)

--- wct.conf.js
+++ wct.conf.js
@@ -3,7 +3,6 @@ module.exports = {
     "sauce": {
       "disabled": true, 
       "browsers": [
-        "Windows 8.1/internet explorer",
         "Windows 10/microsoftedge",
         "OS X 10.12/safari"
     ]
